b4b1cefd1c09495bb270f8fe725da53054b19d30
kate <ccouverte@gmail.com>
2023-12-23 12:43:40 +0300

    first commit

--- dotnet/PgConnect/Program.cs
+++ dotnet/PgConnect/Program.cs
@@ -1,36 +1,47 @@
 ﻿using Npgsql;
+using System;
+using System.IO;
 
-Console.WriteLine("User is ?");
-var userName = Console.ReadLine();
-Console.WriteLine("Password is ?");
-var password = Console.ReadLine();
-Console.WriteLine($"User is {userName}");
-Console.WriteLine($"Password is {password}");
-
-var connectionString = "Host=localhost;Username=kate;Password=Rfnz1601;Database=sdl";
-
-// Проверяем соответствие введенных данных с данными из connection string
-if (userName != "kate" || password != "Rfnz1601")
+class Program
 {
-    Console.WriteLine("Вход неавторизованного пользователя");
-}
-else
-{
-    // Создаем подключение к базе данных
-    await using var dataSource = NpgsqlDataSource.Create(connectionString);
-
-    // Retrieve all rows
-    await using (var cmd = dataSource.CreateCommand("SELECT VERSION();"))
-    await using (var reader = await cmd.ExecuteReaderAsync())
+    static async Task Main(string[] args)
     {
-        while (await reader.ReadAsync())
+        Console.WriteLine("User is ?");
+        var userName = Console.ReadLine();
+        Console.WriteLine("Password is ?");
+        var password = Console.ReadLine();
+
+        Console.WriteLine($"User is {userName}");
+        Console.WriteLine($"Password is {password}");
+
+        StreamReader sr = new StreamReader("../../../sdl.conf");
+        var connectionString = sr.ReadLine();
+
+        Npgsql.NpgsqlConnectionStringBuilder csb = new Npgsql.NpgsqlConnectionStringBuilder(connectionString);
+
+        csb.Username = userName;
+        csb.Password = password;
+
+        try
         {
-            Console.WriteLine(reader.GetString(0));
+            await using var dataSource = NpgsqlDataSource.Create(csb);
+
+            // Retrieve all rows
+            await using (var cmd = dataSource.CreateCommand("SELECT VERSION();"))
+            await using (var reader = await cmd.ExecuteReaderAsync())
+            {
+                while (await reader.ReadAsync())
+                {
+                    Console.WriteLine(reader.GetString(0));
+                }
+            }
+        }
+        catch (Npgsql.PostgresException)
+        {
+            Console.WriteLine("Вход неавторизованного пользователя");
         }
     }
 }
-
-
 
 
 
--- dotnet/PgConnect/obj/Debug/net8.0/PgConnect.AssemblyInfo.cs
+++ dotnet/PgConnect/obj/Debug/net8.0/PgConnect.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PgConnect")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1be1fc89cce245a9377c76db9d7759b32d454c6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PgConnect")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PgConnect")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
